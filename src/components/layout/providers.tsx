d247bdeef8743d626681d000b8a933b201dbb852
PhovThea <phovthea3@gmail.com>
2025-05-22 17:02:25 +0700

    added

--- src/components/layout/providers.tsx
+++ src/components/layout/providers.tsx
@@ -1,5 +1,4 @@
 'use client';
-import { ClerkProvider } from '@clerk/nextjs';
 import { dark } from '@clerk/themes';
 import { useTheme } from 'next-themes';
 import React from 'react';
@@ -18,13 +17,7 @@ export default function Providers({
   return (
     <>
       <ActiveThemeProvider initialTheme={activeThemeValue}>
-        <ClerkProvider
-          appearance={{
-            baseTheme: resolvedTheme === 'dark' ? dark : undefined
-          }}
-        >
           {children}
-        </ClerkProvider>
       </ActiveThemeProvider>
     </>
   );
