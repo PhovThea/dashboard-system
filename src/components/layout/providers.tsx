6f2bc83c553a72bb9feca485b23cba4f72de4153
PhovThea <phovthea3@gmail.com>
2025-05-23 11:26:13 +0700

    added

--- src/components/layout/providers.tsx
+++ src/components/layout/providers.tsx
@@ -1,6 +1,4 @@
 'use client';
-import { dark } from '@clerk/themes';
-import { useTheme } from 'next-themes';
 import React from 'react';
 import { ActiveThemeProvider } from '../active-theme';
 
@@ -12,7 +10,7 @@ export default function Providers({
   children: React.ReactNode;
 }) {
   // we need the resolvedTheme value to set the baseTheme for clerk based on the dark or light theme
-  const { resolvedTheme } = useTheme();
+
 
   return (
     <>
